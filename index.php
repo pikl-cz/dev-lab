4d0bf394a3ee9257358e169d607ae9059839975d
pikl <pikl@oksystem.cz>
2017-08-30 15:09:06 +0200

    ignorelist

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require __DIR__ . '/devLab/FileFolderTree.php';
 
 try {
     $list = new \Assist\FileFolderTree(__DIR__);
-    $list->setIgnoreList(['.', '..', '.idea', '.git', '.gitignore', 'LICENSE', 'README.md', 'node_modules', 'angularjs', 'reactjs', 'devLab']);
+    $list->setIgnoreList(['.', '..', '.idea', '.git', '.gitignore', 'LICENSE', 'README.md', 'node_modules', 'angularjs', 'reactjs', 'devLab', 'nbproject']);
     $list->run();
 } catch (Exception $e) {
     echo $e->getMessage(), "\n";
