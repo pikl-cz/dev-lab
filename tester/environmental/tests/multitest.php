79a4b24a24a1606ec082072ce710d397e22ab91b
filip-jan-pikl <fjpikl@gmail.com>
2017-09-08 15:58:00 +0200

    vzorove testy

---
+++ tester/environmental/tests/multitest.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * pages => amount of visit
+ */
+$pages = [
+    '/produkt/ts-4000/' => 2,
+    '/cs/tag/wamp/' => 3,
+    '/cs/2017/03/' => 1,
+    '/zitkova/rezervace-a-cenik' => 1
+];
+
+$tester = new \Assist\Visitor();
+$tester->setFileToSaveLog('multitest');
+$tester->setUrl('devel', 'http://webar.pikl.cz');
+$tester->setUrl('okac', 'https://www.okbase.cz');
+$tester->setUrl('devel2', 'http://www.hotelkopanice.cz');
+$tester->setUrl('master', 'http://gezedata.cz');
+$tester->setUrl('local', 'http://devlab.dev');
+$tester->setPages($pages);
+$tester->setSitemap('https://www.okbase.cz/sitemap.xml', 1);
+//$tester->setSitemap('mujblogsitemap.xml', 2);
+$tester->find('body');
+$tester->run();
---
+++ tester/environmental/tests/okbase.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Konfigurace testování pro okbase na masteru
+ */
+$tester = new \Assist\Visitor();
+$tester->setFileToSaveLog('okbase');
+$tester->setUrl('master', 'https://www.okbase.cz');
+$tester->setSitemap('https://www.okbase.cz/sitemap.xml', 1);
+$tester->find('oksystem');
+$tester->run();
