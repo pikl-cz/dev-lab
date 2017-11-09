19f4de0ccc78ce1c9a93a3432bc5bd170b66a9a2
pikl <pikl@oksystem.cz>
2017-11-09 16:13:35 +0100

    uprava ignorlistu

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require __DIR__ . '/core/bootstrap.php';
  */
 try {
     $list = new \Assist\FileFolderTree(__DIR__);
-    $list->setIgnoreList(['.', '..', 'phpinfo.php', '.idea', '.git', '.gitignore', 'LICENSE', 'README.md', 'node_modules', 'angularjs', 'reactjs', 'core', 'nbproject', '_experiments', '_extensions']);
+    $list->setIgnoreList(['.', '..', 'phpinfo.php', '.idea', '.git', '.gitignore', 'LICENSE', 'README.md', 'node_modules', 'angularjs', 'reactjs', 'core', 'nbproject', '_experiments', '_extensions', 'logs', 'samples', 'tests']);
     $list->run();
 } catch (Exception $e) {
     echo $e->getMessage(), "\n";
